5659c8ff5383294db287b4d72a27503cb5a47f61
Компьютер <@DESKTOP-RDMTMQN>
2024-12-01 15:44:03 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите числитель (a): ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите знаменатель (b): ");
+int b = int.Parse(Console.ReadLine());
+if (b == 0)
+{
+    Console.WriteLine("Знаменатель не может быть равен нулю.");
+    return;
+}
+int gcd = GCD(a, b);
+int reducedNumerator = a / gcd;
+int reducedDenominator = b / gcd;
+Console.WriteLine($"Сокращенная дробь: {reducedNumerator}/{reducedDenominator}");
+static int GCD(int x, int y)
+{
+    while (y != 0)
+    {
+        int temp = y;
+        y = x % y;
+        x = temp;
+    }
+    return x;
+}
